c9bf5a4e42c722ca429cb128419ce6c91ab8be4e
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2016-03-23 17:42:58 +0300

    TODO: code workaround

--- core/runtime.jvm/src/kotlin/jvm/internal/Lambda.kt
+++ core/runtime.jvm/src/kotlin/jvm/internal/Lambda.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package kotlin.jvm.internal
 
 abstract class Lambda(private val arity: Int) : FunctionImpl() {
-    override fun getArity() = arity
+    override fun getArity(): Int = arity
 
     override fun toString() = Reflection.renderLambdaToString(this)
 }
