7bc69f0a2ff9054bed3d7397193ae2aa108cd58c
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2016-09-12 19:34:26 +0300

    Create NewOverloadingConflictResolver

--- compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/ASTCallResolver.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/ASTCallResolver.kt
@@ -16,35 +16,20 @@
 
 package org.jetbrains.kotlin.resolve.calls
 
-import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.resolve.calls.context.CheckArgumentTypesMode
-import org.jetbrains.kotlin.resolve.calls.inference.SimpleConstraintSystemImpl
-import org.jetbrains.kotlin.resolve.calls.model.CallArgument
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCallArgument
-import org.jetbrains.kotlin.resolve.calls.results.FlatSignature
-import org.jetbrains.kotlin.resolve.calls.results.FlatSignature.Companion.argumentValueType
-import org.jetbrains.kotlin.resolve.calls.results.FlatSignature.Companion.extensionReceiverTypeOrEmpty
-import org.jetbrains.kotlin.resolve.calls.results.OverloadingConflictResolver
-import org.jetbrains.kotlin.resolve.calls.results.TypeSpecificityComparator
 import org.jetbrains.kotlin.resolve.calls.tower.CandidateFactoryProviderForInvoke
 import org.jetbrains.kotlin.resolve.calls.tower.TowerResolver
 import org.jetbrains.kotlin.resolve.calls.tower.createFunctionProcessor
 import org.jetbrains.kotlin.resolve.calls.tower.createVariableAndObjectProcessor
-import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.UnwrappedType
-import org.jetbrains.kotlin.utils.singletonOrEmptyList
 import java.lang.UnsupportedOperationException
-import java.util.*
 
 
 class ASTCallResolver(
         private val towerResolver: TowerResolver,
         private val astCallCompleter: ASTCallCompleter,
-        builtIns: KotlinBuiltIns,
-        specificityComparator: TypeSpecificityComparator,
-        isDescriptorFromSourcePredicate: IsDescriptorFromSourcePredicate
+        private val overloadingConflictResolver: NewOverloadingConflictResolver
 ) {
-    private val overloadingConflictResolver = createOverloadingConflictResolver(builtIns, specificityComparator, isDescriptorFromSourcePredicate)
 
     fun resolveCall(
             callContext: CallContext,
@@ -78,53 +63,6 @@ class ASTCallResolver(
 
         return maximallySpecificCandidates.map {
             astCallCompleter.transformWhenAmbiguity(it)
-        }
-    }
-
-    companion object {
-        private fun createOverloadingConflictResolver(
-                builtIns: KotlinBuiltIns,
-                specificityComparator: TypeSpecificityComparator,
-                isDescriptorFromSourcePredicate: IsDescriptorFromSourcePredicate
-        ) = OverloadingConflictResolver<NewResolutionCandidate>(builtIns, specificityComparator, {
-            if (it is VariableAsFunctionResolutionCandidate) {
-                it.invokeCandidate.descriptorWithFreshTypes
-            }
-            else {
-                (it as SimpleResolutionCandidate).descriptorWithFreshTypes
-            }
-        }, ::SimpleConstraintSystemImpl, Companion::createFlatSignature, { (it as? VariableAsFunctionResolutionCandidate)?.resolvedVariable }, isDescriptorFromSourcePredicate)
-
-        private fun createFlatSignature(candidate: NewResolutionCandidate): FlatSignature<NewResolutionCandidate> {
-            val simpleCandidate = (candidate as? VariableAsFunctionResolutionCandidate)?.invokeCandidate ?: (candidate as SimpleResolutionCandidate)
-
-            val originalDescriptor = simpleCandidate.descriptorWithFreshTypes.original
-            val originalValueParameters = originalDescriptor.valueParameters
-
-            var numDefaults = 0
-            val valueArgumentToParameterType = HashMap<CallArgument, KotlinType>()
-            for ((valueParameter, resolvedValueArgument) in simpleCandidate.argumentMappingByOriginal) {
-                if (resolvedValueArgument is ResolvedCallArgument.DefaultArgument) {
-                    numDefaults++
-                }
-                else {
-                    val originalValueParameter = originalValueParameters[valueParameter.index]
-                    val parameterType = originalValueParameter.argumentValueType
-                    for (valueArgument in resolvedValueArgument.arguments) {
-                        valueArgumentToParameterType[valueArgument] = parameterType
-                    }
-                }
-            }
-
-            return FlatSignature(candidate,
-                                 originalDescriptor.typeParameters,
-                                 valueParameterTypes = originalDescriptor.extensionReceiverTypeOrEmpty() +
-                                                       simpleCandidate.astCall.argumentsInParenthesis.map { valueArgumentToParameterType[it] } +
-                                                       simpleCandidate.astCall.externalArgument?.let { valueArgumentToParameterType[it] }.singletonOrEmptyList(),
-                                 hasExtensionReceiver = originalDescriptor.extensionReceiverParameter != null,
-                                 hasVarargs = originalDescriptor.valueParameters.any { it.varargElementType != null },
-                                 numDefaults = numDefaults)
-
         }
     }
 }
